69f328a7d03774bee68119b139ee1bb527ac7838
AY Asemota <asemotamaki1@gmail.com>
2025-02-15 16:52:45 +0100

    card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,12 +9,12 @@ export default function Product(props: Products) {
   <div>
     <article
      key={props.index}
-     className="hover:scale-[1.1] flex items-end justify-center w-[340px] h-[460px] rounded-xl bg-cover bg-black bg-opacity-90 bg-no-repeat px-8 py-4"
+     className="hover:scale-[1.1] flex items-end justify-start w-[340px] h-[460px] rounded-xl bg-cover bg-black bg-opacity-90 bg-no-repeat px-8 py-4"
      style={{
       backgroundImage: `url('${props.src}')`
      }}
     >
-     <p className="text-white text-lg font-semibold px-4 py-6 font-tight">{props.text}</p>
+     <p className="text-white text-md font-semibold px-4 py-6 font-tight">{props.text}</p>
     </article>
   </div>
  )
