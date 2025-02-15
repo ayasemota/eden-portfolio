e0fa7b8b558b0fd28df2c64d8187f7a8a08e0b1e
AY Asemota <asemotamaki1@gmail.com>
2025-02-15 16:55:31 +0100

    production fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../app/globals.css";
-import Preloader from "@/components/Preloader";
 
 export default function App({ Component, pageProps }: AppProps) {
  return (
@@ -10,7 +9,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <title>Eden Portfolio</title>
     <meta name="description" content="Created With Love - Design by Eden" />
    </Head>
-   {/* <Preloader /> */}
    <Component {...pageProps} />
   </>
  );
